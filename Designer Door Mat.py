fca19715e17575c7cc715d9775dc039115f26f9a
Kalyan_Gorjana <kalyangorjana@gmail.com>
2022-10-09 10:43:01 +0530

    Kalyan

--- Designer Door Mat.py
+++ Designer Door Mat.py
@@ -1,6 +1,36 @@
-thickness = int(input())  # This must be an odd number
-c = 'H'
+# Optimistic Way
+s = input()
+n = int(s[:(s.find(' '))])
+m = n * 3
+num = 1
+for i in range(n):
+    if i < int(n / 2):
+        print(('.|.'*num).center(m, '-'))
+        num += 2
+    if i == int(n / 2):
+        print('WELCOME'.center(m, '-'))
+    if i > int(n / 2):
+        print(('.|.'*num).center(m, '-'))
+        num -= 2
 
-# Top Cone
-for i in range(thickness):
-    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+# #My Logic
+# thickness = input().split()
+# row = int(thickness[0])
+# column = int(thickness[1])
+# # c = '-'
+# d = '.|.'
+# e = 'WELCOME'
+# c_count = 1
+# column = int((column1-3)/2)
+# for i in range(1, row+1):
+#     if i < (int(((row-1)/2)+1)):
+#         print((c*column) + (d*c_count) + (c*column))
+#         column -= 3
+#         c_count += 2
+#     if i == (int(((row-1)/2)+1)):
+#         print(e.center(column1, '-'))
+#     if i > (int(((row-1)/2)+1)):
+#         column += 3
+#         c_count -= 2
+#         print((c*column) + (d*c_count) + (c*column))
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-thickness = int(input())  # This must be an odd number
-c = '*'
+# thickness = int(input())  # This must be an odd number
+# c = '*'
+#
+# # Top Cone
+# for i in range(thickness):
+#     print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
 
-# Top Cone
-for i in range(thickness):
-    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+row = 7
+
+for i in range(1, row):
+    if i < 4:
+        print("kalyan")
